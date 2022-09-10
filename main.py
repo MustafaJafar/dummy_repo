9d21726f48e7e74aff3712f3e8ebf9c7570945de
Mustafa Zarkash <mustafataherzaky@outlook.com>
2022-09-10 16:54:05 +0200

    test square fnctions

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 '''this file contains some math formulas''' 
 import circle
+import square 
 
 radius = 3 
-print("circum = ", circle.circumference(3))
-print("area = ", circle.area(3))
+print("circle circum of raduis 3 = ", circle.circumference(radius))
+print("circle area of raduis 3 = ", circle.area(radius))
+
+length = 5 
+print("square circum of length",length,"= ", square.circumference(length))
+print("square area of length",length,"= ", square.area(length))
